b095feaf74c2c4fe0e3f4757df5eef60bc3e9d59
jg1226 <44550227+jg1226@users.noreply.github.com>
2020-04-25 16:13:11 -0700

    Created new activities for 54321

---
+++ app/src/main/java/com/stanhacks/everythingsokay/SmellActivity.java
@@ -0,0 +1,13 @@
+package com.stanhacks.everythingsokay;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmellActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smell);
+    }
+}
