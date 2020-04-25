09245dfb138c9948293b2775c25963059305e4c2
Emma <emmavanhoogmoed@gmail.com>
2020-04-25 15:14:33 -0700

    added homepage functionality and new base activities

--- app/src/main/java/com/stanhacks/everythingsokay/MainActivity.java
+++ app/src/main/java/com/stanhacks/everythingsokay/MainActivity.java
@@ -2,7 +2,10 @@ package com.stanhacks.everythingsokay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mainTitle = (TextView) findViewById(R.id.textViewAppTitle);
+        mainTitle.setText("Everythings Okay");
+    }
+    public void goToThoughtJournalActivity(View view) {
+        Intent goToActivity = new Intent(MainActivity.this,ThoughtJournalActivity.class);
+        startActivity(goToActivity);
+
+
+    }
+
+    public void goToGroundingExerciseActivity(View view){
+        Intent goToActivity = new Intent(MainActivity.this,GroundingExerciseActivity.class);
+        startActivity(goToActivity);
+    }
+
+    public void goToBreathingInActivity (View view){
+        Intent goToActivity = new Intent(MainActivity.this,BreathingInActivity.class);
+        startActivity(goToActivity);
     }
 }
