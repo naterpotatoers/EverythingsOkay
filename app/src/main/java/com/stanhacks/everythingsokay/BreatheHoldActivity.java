c46ab2bbd95419e12a5e5123214a2c4620753209
Emma <emmavanhoogmoed@gmail.com>
2020-04-25 20:51:35 -0700

    Completed Breathing Exercise

--- app/src/main/java/com/stanhacks/everythingsokay/BreatheHoldActivity.java
+++ app/src/main/java/com/stanhacks/everythingsokay/BreatheHoldActivity.java
@@ -2,13 +2,56 @@ package com.stanhacks.everythingsokay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class BreatheHold extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class BreatheHoldActivity extends AppCompatActivity {
+    TextView t1;
+    CountDownTimer countDownTimer;
+    Timer timer;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breathe_hold);
+      TextView mainTitle = (TextView) findViewById(R.id.BreatheHoldTitle);
+       mainTitle.setText("Hold Your Breath");
+
+        t1 = findViewById(R.id.timer);
+        countDownTimer = new CountDownTimer(6000, 1000) {
+
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+
+                t1.setText(millisUntilFinished / 1000 + "");
+            }
+
+            @Override
+            public void onFinish() {
+                t1.setText("0");
+
+            }
+        }.start();
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(BreatheHoldActivity.this, BreatheOutActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 7000);
+
     }
 }
+
--- app/src/main/java/com/stanhacks/everythingsokay/BreatheOutActivity.java
+++ app/src/main/java/com/stanhacks/everythingsokay/BreatheOutActivity.java
@@ -6,35 +6,30 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
-
-public class BreathingInActivity extends AppCompatActivity {
-    Button b1;
+public class BreatheOutActivity extends AppCompatActivity {
     TextView t1;
     CountDownTimer countDownTimer;
     Timer timer;
-
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_breathing_in);
-        TextView mainTitle = (TextView) findViewById(R.id.BreatheInTitle);
-        mainTitle.setText("Breathe In");
+        setContentView(R.layout.activity_breathe_out2);
+        TextView mainTitle = (TextView) findViewById(R.id.BreatheOutTitle);
+        mainTitle.setText("Breathe Out");
 
-        b1 = findViewById(R.id.buttonBegin);
         t1 = findViewById(R.id.timer);
-
         countDownTimer = new CountDownTimer(5000, 1000) {
+
+
             @Override
             public void onTick(long millisUntilFinished) {
+
 
                 t1.setText(millisUntilFinished / 1000 + "");
             }
@@ -42,33 +37,18 @@ public class BreathingInActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 t1.setText("0");
-                //Toast.makeText(BreathingInActivity.this, "Next", Toast.LENGTH_SHORT).show();
-            }
-        };
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Toast.makeText(BreathingInActivity.this, "start", Toast.LENGTH_SHORT).show();
-                countDownTimer.start();
-
 
             }
-
-        });
+        }.start();
         timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                Intent intent = new Intent(BreathingInActivity.this, BreatheHoldActivity.class);
+                Intent intent = new Intent(BreatheOutActivity.this, BreathingInActivity.class);
                 startActivity(intent);
                 finish();
 
             }
         }, 6000);
-
     }
 }
-
-
-
