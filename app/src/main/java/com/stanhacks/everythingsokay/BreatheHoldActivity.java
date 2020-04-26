d9a57c2d9234a7583d0606a569430b366133d99b
Emma <emmavanhoogmoed@gmail.com>
2020-04-25 19:56:38 -0700

    redone breathing hold activity

--- app/src/main/java/com/stanhacks/everythingsokay/BreatheHoldActivity.java
+++ app/src/main/java/com/stanhacks/everythingsokay/BreatheHoldActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class BreatheOutActivity extends AppCompatActivity {
+public class BreatheHold extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_breathe_out);
+        setContentView(R.layout.activity_breathe_hold);
     }
 }
