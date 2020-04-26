e938eb6876b80387b0ec1f8e01a88bd4bf2f334b
naterpotatoers <nate.garza@hotmail.com>
2020-04-25 19:58:35 -0700

    journal activities initial

--- app/src/main/java/com/stanhacks/everythingsokay/NewEntryActivity.java
+++ app/src/main/java/com/stanhacks/everythingsokay/NewEntryActivity.java
@@ -2,7 +2,9 @@ package com.stanhacks.everythingsokay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewEntryActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class NewEntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_entry);
+    }
+
+    public void goToGoodMemoriesActivity(View view){
+        Intent goToActivity = new Intent(NewEntryActivity.this, GoodMemoriesActivity.class);
+        startActivity(goToActivity);
     }
 }
--- app/src/main/java/com/stanhacks/everythingsokay/ThoughtJournalActivity.java
+++ app/src/main/java/com/stanhacks/everythingsokay/ThoughtJournalActivity.java
@@ -2,7 +2,9 @@ package com.stanhacks.everythingsokay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ThoughtJournalActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class ThoughtJournalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thought_journal);
+    }
+
+    public void goToNewEntryActivity(View view){
+        Intent goToActivity = new Intent(ThoughtJournalActivity.this,NewEntryActivity.class);
+        startActivity(goToActivity);
+    }
+
+    public void goToGoodMemoriesActivity(View view){
+        Intent goToActivity = new Intent(ThoughtJournalActivity.this, GoodMemoriesActivity.class);
+        startActivity(goToActivity);
     }
 }
